0e860a8a1a5072f853b7f4f881c4e9e9c6246bfa
nairaghvaladze <120264867+nairaghvaladze@users.noreply.github.com>
2023-07-20 03:07:52 +0400

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,141 @@
+
+document.getElementById("img").src="image/Image (5).png";
+
+
+var person = { firstName:"Kate", lastName:"b." };
+document.getElementById("text").innerHTML=person.firstName;
+document.getElementById("text").style.marginTop="5px";
+document.getElementById("text").style.marginLeft="5px";
+document.getElementById("text").style.fontWeight="600";
+
+document.getElementById("text1").innerHTML=person.lastName;
+document.getElementById("text1").style.marginTop="5px";
+document.getElementById("text1").style.marginLeft="5px";
+document.getElementById("text1").style.fontWeight="600";
+
+var title=["Headset", "Controllers", "Base Stations"];
+var subtitle=["Fidelity first. See more than ever before", "Your hands, in Virtual Reality", "Best-in-industry   room-scale tracking"];
+var description=["We've pushed visual, audio, and ergonomic technologies to create the best-in-class VR experience.",
+"Designed from the ground up to enable natural interactions, high-fidelity hand presence, and long-term comfort.",
+"The highest quality tracking has gotten even better with Base Station 2.0, now with improved range, field of view, and scalability."];
+var cocount=349;
+var shcount=49500;
+
+document.getElementById("img5").src="image/Icon (9).png";
+document.getElementById("img4").src="image/Image (2).png";
+document.getElementById("text4").innerHTML= title[0];
+document.getElementById("text4").style.color="#4CD964";
+document.getElementById("h2").innerHTML=subtitle[0];
+document.getElementById("text5").innerHTML=description[0];
+document.getElementById("span1").innerHTML=cocount;
+document.getElementById("img6").src="image/Icon (8).png";
+document.getElementById("span2").innerHTML=shcount;
+document.getElementById("img7").src="image/Icon (7).png";
+
+document.getElementById("text41").innerHTML=title[1];
+document.getElementById("text41").style.color="#FFFFFF";
+document.getElementById("h21").innerHTML=subtitle[1];
+document.getElementById("h21").style.color="#FFFFFF";
+document.getElementById("h21").style.fontSize="40px";
+document.getElementById("text51").innerHTML=description[1];
+document.getElementById("text51").style.color="#FFFFFF";
+document.getElementById("img51").src="image/Icon (9).png";
+document.getElementById("span11").innerHTML=cocount;
+document.getElementById("span11").style.color="#FFFFFF";
+document.getElementById("img61").src="image/Icon (8).png";
+document.getElementById("span21").innerHTML=shcount;
+document.getElementById("span21").style.color="#FFFFFF";
+document.getElementById("img71").src="image/Icon (7).png";
+
+
+document.getElementById("img41").src="image/Image (3).png";
+document.getElementById("text42").innerHTML=title[2];
+document.getElementById("text42").style.color="#4CD964";
+document.getElementById("h22").innerHTML=subtitle[2];
+document.getElementById("h22").style.fontSize="36px";
+document.getElementById("text52").innerHTML=description[2];
+document.getElementById("img52").src="image/Icon (9).png";
+document.getElementById("span12").innerHTML=cocount;
+document.getElementById("img62").src="image/Icon (8).png";
+document.getElementById("span22").innerHTML=shcount;
+document.getElementById("img72").src="image/Icon (7).png";
+
+document.getElementById("img46").src="image/Image (2).png";
+document.getElementById("text43").innerHTML= title[0];
+document.getElementById("text43").style.color="#4CD964";
+document.getElementById("h23").innerHTML=subtitle[0];
+document.getElementById("text53").innerHTML=description[0];
+document.getElementById("img53").src="image/Icon (9).png";
+document.getElementById("span13").innerHTML=cocount;
+document.getElementById("img63").src="image/Icon (8).png";
+document.getElementById("span23").innerHTML=shcount;
+document.getElementById("img73").src="image/Icon (7).png";
+
+
+
+
+document.getElementById("text44").innerHTML= title[1];
+document.getElementById("text44").style.color="#FFFFFF";
+document.getElementById("h24").innerHTML=subtitle[1];
+document.getElementById("h24").style.color="#FFFFFF";
+document.getElementById("h24").style.fontSize="40px";
+document.getElementById("text54").innerHTML=description[1];
+document.getElementById("img54").src="image/Icon (9).png";
+document.getElementById("span14").innerHTML=cocount;
+document.getElementById("span14").style.color="#FFFFFF";
+document.getElementById("img64").src="image/Icon (8).png";
+document.getElementById("span24").innerHTML=shcount;
+document.getElementById("span24").style.color="#FFFFFF";
+document.getElementById("img74").src="image/Icon (7).png";
+
+
+
+
+
+
+
+
+
+document.getElementById("img47").src="image/Image (2).png";
+document.getElementById("text45").innerHTML= title[2];
+document.getElementById("text45").style.color="#4CD964";
+document.getElementById("h25").innerHTML=subtitle[2];
+document.getElementById("h25").style.fontSize="36px";
+document.getElementById("text55").innerHTML=description[2];
+document.getElementById("img55").src="image/Icon (9).png";
+document.getElementById("span15").innerHTML=cocount;
+document.getElementById("img65").src="image/Icon (8).png";
+document.getElementById("span25").innerHTML=shcount;
+document.getElementById("img75").src="image/Icon (7).png";
+
+
+
+
+
+
+var created="© 2019 crowdfund. All Rights Reserved";
+
+document.getElementById("text2").innerHTML=created;
+document.getElementById("text2").style.marginLeft="15px";
+document.getElementById("text2").style.fontWeight="bold";
+var foot=["CONTACT", "HELP", "TERMS OF USE", "PRIVACY POLICY"];
+document.getElementById("text_3").style.fontWeight="bold";
+document.getElementById("text3").innerHTML=foot[0];
+document.getElementById("text31").innerHTML=foot[1];
+document.getElementById("text32").innerHTML=foot[2];
+document.getElementById("text33").innerHTML=foot[3];
+
+document.getElementById("img1").src="image/Twitter (3).png";
+
+document.getElementById("img1").style.marginRight="10px";
+
+document.getElementById("img2").src="image/Facebook (3).png";
+document.getElementById("img2").style.marginRight="10px";
+document.getElementById("img1").style.href="www.google.com";
+document.getElementById("img3").src="image/YouTube (2).png";
+document.getElementById("img3").style.marginRight="10px";
+
+
+
+
+
